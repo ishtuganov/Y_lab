6a5b1132a4c8da66c9c98645e86d70b7d7ef826a
ishtuganov <146653031+ishtuganov@users.noreply.github.com>
2024-06-24 09:33:43 +0400

    Add files via upload

---
+++ practics/practics/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace practics
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            buttonTask6 = new Button();
+            buttonTask5 = new Button();
+            buttonTask4 = new Button();
+            buttonTask3 = new Button();
+            buttonTask2 = new Button();
+            buttonTask1 = new Button();
+            panel2 = new Panel();
+            button2dArray = new Button();
+            numericUpDownTopRange = new NumericUpDown();
+            numericUpDownBottomRange = new NumericUpDown();
+            numericUpDown2dHeight = new NumericUpDown();
+            numericUpDown2dWidth = new NumericUpDown();
+            numericUpDown1dSize = new NumericUpDown();
+            labelValuesRange = new Label();
+            label2dSize = new Label();
+            label1dSize = new Label();
+            button1dArray = new Button();
+            textBoxArray = new TextBox();
+            panel3 = new Panel();
+            buttonExecute = new Button();
+            textBoxResultArray = new TextBox();
+            labelSelectedTask = new Label();
+            textBoxSelectedTask = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTopRange).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownBottomRange).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2dHeight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2dWidth).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1dSize).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(buttonTask6);
+            panel1.Controls.Add(buttonTask5);
+            panel1.Controls.Add(buttonTask4);
+            panel1.Controls.Add(buttonTask3);
+            panel1.Controls.Add(buttonTask2);
+            panel1.Controls.Add(buttonTask1);
+            panel1.Location = new Point(21, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(166, 330);
+            panel1.TabIndex = 0;
+            // 
+            // buttonTask6
+            // 
+            buttonTask6.Location = new Point(3, 278);
+            buttonTask6.Name = "buttonTask6";
+            buttonTask6.Size = new Size(160, 39);
+            buttonTask6.TabIndex = 5;
+            buttonTask6.Text = "задание 6";
+            buttonTask6.UseVisualStyleBackColor = true;
+            // 
+            // buttonTask5
+            // 
+            buttonTask5.Location = new Point(3, 223);
+            buttonTask5.Name = "buttonTask5";
+            buttonTask5.Size = new Size(160, 39);
+            buttonTask5.TabIndex = 4;
+            buttonTask5.Text = "задание 5";
+            buttonTask5.UseVisualStyleBackColor = true;
+            // 
+            // buttonTask4
+            // 
+            buttonTask4.Location = new Point(3, 170);
+            buttonTask4.Name = "buttonTask4";
+            buttonTask4.Size = new Size(160, 38);
+            buttonTask4.TabIndex = 3;
+            buttonTask4.Text = "задание 4";
+            buttonTask4.UseVisualStyleBackColor = true;
+            // 
+            // buttonTask3
+            // 
+            buttonTask3.Location = new Point(3, 119);
+            buttonTask3.Name = "buttonTask3";
+            buttonTask3.Size = new Size(160, 38);
+            buttonTask3.TabIndex = 2;
+            buttonTask3.Text = "задание 3";
+            buttonTask3.UseVisualStyleBackColor = true;
+            // 
+            // buttonTask2
+            // 
+            buttonTask2.Location = new Point(3, 64);
+            buttonTask2.Name = "buttonTask2";
+            buttonTask2.Size = new Size(160, 39);
+            buttonTask2.TabIndex = 1;
+            buttonTask2.Text = "задание 2";
+            buttonTask2.UseVisualStyleBackColor = true;
+            // 
+            // buttonTask1
+            // 
+            buttonTask1.Location = new Point(3, 11);
+            buttonTask1.Name = "buttonTask1";
+            buttonTask1.Size = new Size(160, 39);
+            buttonTask1.TabIndex = 0;
+            buttonTask1.Text = "задание 1";
+            buttonTask1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button2dArray);
+            panel2.Controls.Add(numericUpDownTopRange);
+            panel2.Controls.Add(numericUpDownBottomRange);
+            panel2.Controls.Add(numericUpDown2dHeight);
+            panel2.Controls.Add(numericUpDown2dWidth);
+            panel2.Controls.Add(numericUpDown1dSize);
+            panel2.Controls.Add(labelValuesRange);
+            panel2.Controls.Add(label2dSize);
+            panel2.Controls.Add(label1dSize);
+            panel2.Controls.Add(button1dArray);
+            panel2.Controls.Add(textBoxArray);
+            panel2.Location = new Point(205, 21);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(592, 208);
+            panel2.TabIndex = 1;
+            // 
+            // button2dArray
+            // 
+            button2dArray.Location = new Point(69, 171);
+            button2dArray.Name = "button2dArray";
+            button2dArray.Size = new Size(129, 23);
+            button2dArray.TabIndex = 10;
+            button2dArray.Text = "двумерный";
+            button2dArray.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownTopRange
+            // 
+            numericUpDownTopRange.Location = new Point(491, 94);
+            numericUpDownTopRange.Minimum = new decimal(new int[] { 100, 0, 0, int.MinValue });
+            numericUpDownTopRange.Name = "numericUpDownTopRange";
+            numericUpDownTopRange.Size = new Size(92, 23);
+            numericUpDownTopRange.TabIndex = 9;
+            numericUpDownTopRange.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // numericUpDownBottomRange
+            // 
+            numericUpDownBottomRange.Location = new Point(390, 94);
+            numericUpDownBottomRange.Minimum = new decimal(new int[] { 100, 0, 0, int.MinValue });
+            numericUpDownBottomRange.Name = "numericUpDownBottomRange";
+            numericUpDownBottomRange.Size = new Size(95, 23);
+            numericUpDownBottomRange.TabIndex = 8;
+            numericUpDownBottomRange.Value = new decimal(new int[] { 10, 0, 0, int.MinValue });
+            // 
+            // numericUpDown2dHeight
+            // 
+            numericUpDown2dHeight.Location = new Point(390, 59);
+            numericUpDown2dHeight.Name = "numericUpDown2dHeight";
+            numericUpDown2dHeight.Size = new Size(95, 23);
+            numericUpDown2dHeight.TabIndex = 7;
+            // 
+            // numericUpDown2dWidth
+            // 
+            numericUpDown2dWidth.Location = new Point(491, 59);
+            numericUpDown2dWidth.Name = "numericUpDown2dWidth";
+            numericUpDown2dWidth.Size = new Size(92, 23);
+            numericUpDown2dWidth.TabIndex = 6;
+            // 
+            // numericUpDown1dSize
+            // 
+            numericUpDown1dSize.Location = new Point(491, 21);
+            numericUpDown1dSize.Name = "numericUpDown1dSize";
+            numericUpDown1dSize.Size = new Size(92, 23);
+            numericUpDown1dSize.TabIndex = 5;
+            // 
+            // labelValuesRange
+            // 
+            labelValuesRange.AutoSize = true;
+            labelValuesRange.Location = new Point(258, 96);
+            labelValuesRange.Name = "labelValuesRange";
+            labelValuesRange.Size = new Size(115, 15);
+            labelValuesRange.TabIndex = 0;
+            labelValuesRange.Text = "Диапазон значений";
+            // 
+            // label2dSize
+            // 
+            label2dSize.AutoSize = true;
+            label2dSize.Location = new Point(258, 59);
+            label2dSize.Name = "label2dSize";
+            label2dSize.Size = new Size(116, 15);
+            label2dSize.TabIndex = 0;
+            label2dSize.Text = "Размер двумерного";
+            // 
+            // label1dSize
+            // 
+            label1dSize.AutoSize = true;
+            label1dSize.Location = new Point(258, 23);
+            label1dSize.Name = "label1dSize";
+            label1dSize.Size = new Size(125, 15);
+            label1dSize.TabIndex = 0;
+            label1dSize.Text = "Размер одномерного";
+            // 
+            // button1dArray
+            // 
+            button1dArray.Location = new Point(69, 136);
+            button1dArray.Name = "button1dArray";
+            button1dArray.Size = new Size(129, 23);
+            button1dArray.TabIndex = 1;
+            button1dArray.Text = "одномерный";
+            button1dArray.UseVisualStyleBackColor = true;
+            // 
+            // textBoxArray
+            // 
+            textBoxArray.Location = new Point(30, 11);
+            textBoxArray.Multiline = true;
+            textBoxArray.Name = "textBoxArray";
+            textBoxArray.Size = new Size(205, 115);
+            textBoxArray.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(buttonExecute);
+            panel3.Controls.Add(textBoxResultArray);
+            panel3.Controls.Add(labelSelectedTask);
+            panel3.Controls.Add(textBoxSelectedTask);
+            panel3.Location = new Point(205, 244);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(592, 198);
+            panel3.TabIndex = 2;
+            // 
+            // buttonExecute
+            // 
+            buttonExecute.Location = new Point(319, 14);
+            buttonExecute.Name = "buttonExecute";
+            buttonExecute.Size = new Size(90, 23);
+            buttonExecute.TabIndex = 3;
+            buttonExecute.Text = "Выполнить";
+            buttonExecute.UseVisualStyleBackColor = true;
+            // 
+            // textBoxResultArray
+            // 
+            textBoxResultArray.Location = new Point(319, 43);
+            textBoxResultArray.Multiline = true;
+            textBoxResultArray.Name = "textBoxResultArray";
+            textBoxResultArray.Size = new Size(264, 144);
+            textBoxResultArray.TabIndex = 2;
+            // 
+            // labelSelectedTask
+            // 
+            labelSelectedTask.AutoSize = true;
+            labelSelectedTask.Location = new Point(18, 18);
+            labelSelectedTask.Name = "labelSelectedTask";
+            labelSelectedTask.Size = new Size(52, 15);
+            labelSelectedTask.TabIndex = 1;
+            labelSelectedTask.Text = "Задание";
+            // 
+            // textBoxSelectedTask
+            // 
+            textBoxSelectedTask.Location = new Point(18, 42);
+            textBoxSelectedTask.Multiline = true;
+            textBoxSelectedTask.Name = "textBoxSelectedTask";
+            textBoxSelectedTask.Size = new Size(269, 145);
+            textBoxSelectedTask.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(817, 453);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownTopRange).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownBottomRange).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2dHeight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2dWidth).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1dSize).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button buttonTask6;
+        private Button buttonTask5;
+        private Button buttonTask4;
+        private Button buttonTask3;
+        private Button buttonTask2;
+        private Button buttonTask1;
+        private Panel panel2;
+        private Button button1dArray;
+        private TextBox textBoxArray;
+        private NumericUpDown numericUpDown2dHeight;
+        private NumericUpDown numericUpDown2dWidth;
+        private NumericUpDown numericUpDown1dSize;
+        private Label labelValuesRange;
+        private Label label2dSize;
+        private Label label1dSize;
+        private NumericUpDown numericUpDownTopRange;
+        private NumericUpDown numericUpDownBottomRange;
+        private Button button2dArray;
+        private Panel panel3;
+        private Button buttonExecute;
+        private TextBox textBoxResultArray;
+        private Label labelSelectedTask;
+        private TextBox textBoxSelectedTask;
+    }
+}
